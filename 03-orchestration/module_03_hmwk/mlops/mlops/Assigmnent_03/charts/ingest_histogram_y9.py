878108a4d9411ceb67c76c004ebc6b46573a48e7
Ismail <t.sukanmi@gmail.com>
2024-06-10 18:01:29 +0000

    assignment update

--- 03-orchestration/module_03_hmwk/mlops/mlops/Assigmnent_03/charts/ingest_histogram_y9.py
+++ 03-orchestration/module_03_hmwk/mlops/mlops/Assigmnent_03/charts/ingest_histogram_y9.py
@@ -12,5 +12,5 @@ if isinstance(df_1, list) and len(df_1) >= 1:
     df_1 = item
 
 columns = df_1.columns
-col = 'trip_distance'
-x = df_1[df_1[col] <= 20][col]
+col = list(filter(lambda x: df_1[x].dtype == float or df_1[x].dtype == int, columns))[0]
+x = df_1[col]
