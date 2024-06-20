f93e7f8f7113010293ed456033482136a75d33fe
Ismail <t.sukanmi@gmail.com>
2024-06-20 12:33:01 +0000

    updating module 04 assignment

--- 04-deployment/batch_deployment_ass/score.py
+++ 04-deployment/batch_deployment_ass/score.py
@@ -2,21 +2,16 @@
 # coding: utf-8
 
 import sys
+import os
+from pathlib import Path
 import pickle
 import pandas as pd
-import uuid
-
-
+from flask import Flask, jsonify, request
 
 with open('model.bin', 'rb') as f_in:
     dv, model = pickle.load(f_in)
 
-
 def read_data(filename):
-    taxi_type = sys.argv[1] #'yellow'
-    year = int(sys.argv[2]) #2023
-    month = int(sys.argv[3]) #3
-
     categorical = ['PULocationID', 'DOLocationID']
     df = pd.read_parquet(filename)
     
@@ -30,8 +25,7 @@ def read_data(filename):
     
     return df
 
-
-def prepare_df_dicts(df:pd.DataFrame):
+def prepare_df_dicts(df: pd.DataFrame):
     categorical = ['PULocationID', 'DOLocationID']
     dicts = df[categorical].to_dict(orient='records')
     return dicts
@@ -50,6 +44,10 @@ def apply_model(input_file, model, output_file):
     df_result['predicted_duration'] = y_pred
     print(f"The mean of predicted duration:{df_result['predicted_duration'].mean()}")
 
+      # Ensure the output directory exists
+    output_path = Path(output_file).parent
+    output_path.mkdir(parents=True, exist_ok=True)
+
     # saving the dataframe as parquet file
     df_result.to_parquet(
         output_file,
@@ -57,10 +55,10 @@ def apply_model(input_file, model, output_file):
         compression=None,
         index=False
     )
-   
 
 
-def run():
+
+def run(taxi_type, year, month ):
 
     taxi_type = sys.argv[1] #'yellow'
     year = int(sys.argv[2]) #2023
@@ -68,19 +66,33 @@ def run():
 
     input_file = f'https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_2023-03.parquet'
     output_file = f'output/{taxi_type}/{year:04d}-{month:02d}.parquet'
-    
+
     apply_model(
         input_file=input_file,
         model=model,
         output_file=output_file
     )
 
+app = Flask('Duration Predictor')
 
+@app.route('/predict', methods=['POST'])
+def predict_endpoint():
+    ride = request.get_json()
+    dicts = prepare_df_dicts(pd.DataFrame([ride]))
+    X_val = dv.transform(dicts)
+    pred = model.predict(X_val)[0]
 
+    result = {
+        'The estimated duration is': pred
+    }
 
-if __name__== "__main__":
-    run()
+    return jsonify(result)
 
-
-
-
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        taxi_type = sys.argv[1]
+        year = int(sys.argv[2])
+        month = int(sys.argv[3])
+        run(taxi_type, year, month)
+    else:
+        app.run(debug=True, host='0.0.0.0', port=9696)
